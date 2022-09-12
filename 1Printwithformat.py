499875def0115c7f3f6afc3ddfbddd0e8cecfaa3
ProgrammingWhale <programmingwhale@gmail.com>
2022-09-12 10:31:31 -0300

    primeiro projeto

---
+++ 1Printwithformat.py
@@ -0,0 +1,6 @@
+# Programa simples para testar o uso de .formatm input e print
+#Simple program to test the use of .format
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+peso = input("Quanto você pesa? ")
+print("Olá {}, você tem {} anos e pesa {} kgs! ".format(nome,idade,peso))
